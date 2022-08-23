3df233126cc0b187f7b63a73622ca1a959055e3f
Bakhman <begaev7@gmail.com>
2022-08-23 20:26:31 +0300

    Add files via upload

---
+++ src/main/java/com/bakh/springsecurity312/controllers/AdminController.java
@@ -0,0 +1,73 @@
+package com.bakh.springsecurity312.controllers;
+
+import com.bakh.springsecurity312.models.Role;
+import com.bakh.springsecurity312.models.User;
+import com.bakh.springsecurity312.services.RoleServiceImpl;
+import com.bakh.springsecurity312.services.UserServiceImpl;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+/**
+ * @author Bakhmai Begaev
+ */
+@Controller
+@RequestMapping()
+public class AdminController {
+
+    private final UserServiceImpl userService;
+    private final RoleServiceImpl roleService;
+
+    public AdminController(UserServiceImpl userService, RoleServiceImpl roleService) {
+        this.userService = userService;
+        this.roleService = roleService;
+    }
+
+    @GetMapping("/")
+    public String homePage() {
+        return "homePage";
+    }
+
+    @GetMapping("/admin")
+    public String getUsers(Model model) {
+        List<User> users = userService.findAllUsers();
+        model.addAttribute("users", users);
+        return "admin/user-list";
+    }
+
+    @GetMapping("/createUser")
+    public String userForm(Model model) {
+        List<Role> roles = roleService.getAllRoles();
+        model.addAttribute("user", new User());
+        model.addAttribute("rolesList", roles);
+        return  "admin/create-user";
+    }
+
+    @PostMapping("/createUser")
+    public String create(@ModelAttribute("user")  User user) {
+        userService.save(user);
+        return "redirect:/admin";
+    }
+
+    @GetMapping("/update/{id}")
+    public String updateUserForm(@PathVariable Long id, Model model) {
+        List<Role> roles = roleService.getAllRoles();
+        model.addAttribute("user", userService.findUserById(id));
+        model.addAttribute("rolesList", roles);
+        return "admin/update-user";
+    }
+
+    @PatchMapping("/update/{id}")
+    public String update(@PathVariable Long id, User user) {
+        userService.editUser(user);
+        return "redirect:/admin";
+    }
+
+    @GetMapping("/delete/{id}")
+    public String delete(@PathVariable Long id) {
+        userService.deleteUserById(id);
+        return "redirect:/admin";
+    }
+}
---
+++ src/main/java/com/bakh/springsecurity312/controllers/UserController.java
@@ -0,0 +1,34 @@
+package com.bakh.springsecurity312.controllers;
+
+import com.bakh.springsecurity312.models.User;
+import com.bakh.springsecurity312.services.UserDetailsServiceImpl;
+import com.bakh.springsecurity312.services.UserServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.security.Principal;
+
+/**
+ * @author Bakhmai Begaev
+ */
+@Controller
+public class UserController {
+    private final UserServiceImpl userService;
+
+    @Autowired
+    public UserController(UserServiceImpl userService, UserDetailsServiceImpl userDetailsServiceImpl) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/user")
+    public String getUser(Model model) {
+        User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        Long userId = user.getId();
+        user = userService.findUserById(userId);
+        model.addAttribute("user", user);
+        return "get-user";
+    }
+}
