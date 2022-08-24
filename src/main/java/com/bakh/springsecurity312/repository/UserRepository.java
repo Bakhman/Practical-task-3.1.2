263b877b8b76ac5b4cf366ed32bc2cb203bb0658
Bakhman <begaev7@gmail.com>
2022-08-25 00:43:13 +0300

    Update UserRepository.java

--- src/main/java/com/bakh/springsecurity312/repository/UserRepository.java
+++ src/main/java/com/bakh/springsecurity312/repository/UserRepository.java
@@ -1,9 +1,7 @@
-package com.bakh.springsecurity312.repositories;
+package com.bakh.springsecurity312.repository;
 
-import com.bakh.springsecurity312.models.User;
-import org.springframework.data.jpa.repository.EntityGraph;
+import com.bakh.springsecurity312.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import javax.transaction.Transactional;
