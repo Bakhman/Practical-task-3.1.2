a7af96b40ee0e6f8f746b97f77f67a772a6e46ec
Bakhman <begaev7@gmail.com>
2022-08-25 00:45:22 +0300

    Update UserServiceImpl.java

--- src/main/java/com/bakh/springsecurity312/service/UserServiceImpl.java
+++ src/main/java/com/bakh/springsecurity312/service/UserServiceImpl.java
@@ -1,8 +1,8 @@
-package com.bakh.springsecurity312.services;
+package com.bakh.springsecurity312.service;
 
-import com.bakh.springsecurity312.models.User;
-import com.bakh.springsecurity312.repositories.RoleRepository;
-import com.bakh.springsecurity312.repositories.UserRepository;
+import com.bakh.springsecurity312.model.User;
+import com.bakh.springsecurity312.repository.RoleRepository;
+import com.bakh.springsecurity312.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.security.core.userdetails.UserDetails;
