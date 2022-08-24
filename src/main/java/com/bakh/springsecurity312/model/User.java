4bfc3a212939640dfb0a2040e6f03026dca04165
Bakhman <begaev7@gmail.com>
2022-08-25 00:09:16 +0300

    Update User.java

--- src/main/java/com/bakh/springsecurity312/model/User.java
+++ src/main/java/com/bakh/springsecurity312/model/User.java
@@ -15,9 +15,6 @@ import java.util.Set;
 @Entity
 @Table(name="users_table")
 public class User implements UserDetails {
-
-    public static final String ROLE = "User[roles]";
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column
@@ -31,28 +28,13 @@ public class User implements UserDetails {
     @Column
     private String email;
 
-    @ManyToMany(fetch = FetchType.EAGER)
+    @ManyToMany(fetch = FetchType.LAZY)
     @JoinTable(name = "user_roles",
             joinColumns = @JoinColumn(name = "user_id"),
             inverseJoinColumns = @JoinColumn(name = "role_id"))
     private Set<Role> roles = new HashSet<>();
 
     public User() {
-    }
-
-    public User(String name) {
-        this.name = name;
-    }
-
-    public User(String name, String email) {
-        this.name = name;
-        this.email = email;
-    }
-
-    public User(String username, String name,  String email) {
-        this.username = username;
-        this.name = name;
-        this.email = email;
     }
 
     public User(String username, String password, String name, String email, Set<Role> roles) {
@@ -138,20 +120,14 @@ public class User implements UserDetails {
         return true;
     }
 
-    @Override
+     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (!(o instanceof User user)) return false;
 
-        if (getId() != null ? !getId().equals(user.getId()) : user.getId() != null) return false;
-        if (getUsername() != null ? !getUsername().equals(user.getUsername()) : user.getUsername() != null)
-            return false;
-        if (getPassword() != null ? !getPassword().equals(user.getPassword()) : user.getPassword() != null)
-            return false;
-        if (getName() != null ? !getName().equals(user.getName()) : user.getName() != null) return false;
-        if (getEmail() != null ? !getEmail().equals(user.getEmail()) : user.getEmail() != null) return false;
-        return getRoles() != null ? getRoles().equals(user.getRoles()) : user.getRoles() == null;
+        return getId() != null ? getId().equals(user.getId()) : user.getId() == null;
     }
+
 
     @Override
     public int hashCode() {
